8161b2130db11cdd1741b1cb09d25720e250ba98
Giulia Abate <giulia.abate1101@gmail.com>
2024-12-31 18:44:34 +0100

    Aggiornamento form v2

--- src/pages/Contatti.js
+++ src/pages/Contatti.js
@@ -35,7 +35,7 @@ function Contatti(){
 
             <div className="form-contatto col-xs-12 col-lg-6">
                 <form name="contact" method="POST" netlify-honeypot="bot-field" hidden>
-                    <input type="hidden" name="form-name" value="contact" />
+                    <input type="hidden" name="contact" value="contact" />
                     <div className="input-container">
                         <label>Nome</label>
                         <input type="text" name="name" />
