abfcb80dc913f2f7575ce1b867fef269769c4cd3
GiuliaAbate <giulia.abate1101@gmail.com>
2025-01-01 21:06:51 +0100

    Aggiornamento Contatti.js

--- src/pages/Contatti.js
+++ src/pages/Contatti.js
@@ -34,8 +34,8 @@ function Contatti(){
             </div>
 
             <div className="form-contatto col-xs-12 col-lg-6">
-                <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
-                    <input type="hidden" name="contact" value="contact" />
+                <form name="contact" method="POST">
+                    <input type="hidden" name="form-name" value="contact" />
                     <div className="input-container">
                         <label>Nome</label>
                         <input type="text" name="name" />
